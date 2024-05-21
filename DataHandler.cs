ba0ceb930ee4c6af62cd9fac62848a1557ef4096
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-05-21 13:00:23 +0200

    Initial commit

---
+++ DataHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_44905165
+{
+    internal class DataHandler
+    {
+        private SqlConnection conn;
+
+        public DataHandler()
+        {
+            try
+            {
+                conn = new SqlConnection($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BrightonMedical.mdf;Integrated Security=True");
+            }
+            catch (Exception ex)
+            {
+                // error message
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
